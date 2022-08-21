6f53fed0d5b0489a3648c2da01c38e0a1e46499d
곽의진 <yuijin0909@cashwalk.io>
2022-08-21 16:54:02 +0900

    feat(Base): BaseActivity, BaseFragment 구성

---
+++ app/src/main/java/com/study/financialrefrigerator/base/BaseFragment.kt
@@ -0,0 +1,25 @@
+package com.study.financialrefrigerator.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<Binding : ViewDataBinding> : Fragment() {
+    private var _binding: Binding? = null
+    open val binding = _binding!!
+
+    abstract val layoutId: Int
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        return binding.root
+    }
+}

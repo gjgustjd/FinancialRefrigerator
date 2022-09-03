25a435608521bb34bed88bf9dee3e1fef5f762de
gjgustjd <gjgustjd70@gmail.com>
2022-09-03 16:58:04 +0900

    feat(fragment):식재료 DB 가져와 RecyclerView로 표시

--- app/src/main/java/com/study/financialrefrigerator/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/study/financialrefrigerator/presentation/home/HomeViewModel.kt
@@ -1,12 +1,15 @@
 package com.study.financialrefrigerator.presentation.home
 
+import android.content.Context
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.study.financialrefrigerator.model.RefriegeratorRepository
 import com.study.financialrefrigerator.model.ingredient.IngredientItem
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,12 +17,11 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(private val repository: RefriegeratorRepository):
     ViewModel() {
     private val _homeIngredients = MutableLiveData<List<IngredientItem>>()
-    val homeIngredients by lazy { _homeIngredients.value }
+    val homeIngredients:LiveData<List<IngredientItem>> =  _homeIngredients
 
     fun setupIngrediestsData() {
         viewModelScope.launch {
             _homeIngredients.value = repository.getAllIngredient()
-            Log.i("HomeViewModel", _homeIngredients.value.toString())
         }
     }
 

44e092640b980d3b249eb1b91bb579b0863c4e4d
gjgustjd <gjgustjd70@gmail.com>
2022-09-03 16:57:36 +0900

    feat(model):DB 미리 채우기 구현

--- app/src/main/java/com/study/financialrefrigerator/model/ingredient/IngredientItem.kt
+++ app/src/main/java/com/study/financialrefrigerator/model/ingredient/IngredientItem.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "ingredients")
 data class IngredientItem(
-    @PrimaryKey val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name:String,
     val quantity:Int,
     val unit:String,

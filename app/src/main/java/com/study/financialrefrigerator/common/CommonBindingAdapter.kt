c11cbdcb6193e084af34879729dcf807c2c33755
gjgustjd <gjgustjd70@gmail.com>
2022-09-04 01:48:48 +0900

    CommonBindingAdapter.kt 추가
    레시피 이미지 url 데이터바인딩하여 바인딩 어댑터로 glide 사용하여 이미지 로드

---
+++ app/src/main/java/com/study/financialrefrigerator/common/CommonBindingAdapter.kt
@@ -0,0 +1,20 @@
+package com.study.financialrefrigerator.common
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+
+object CommonBindingAdapter {
+
+    @BindingAdapter("app:imageUrl")
+    @JvmStatic
+    fun loadImage(imageView: ImageView, url: String) {
+        Glide.with(imageView.context)
+            .load(url)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .apply(RequestOptions().fitCenter())
+            .into(imageView)
+    }
+}

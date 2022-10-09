33c1726015c944f9984f826b9ccc7cc5db326f4e
gjgustjd <gjgustjd70@gmail.com>
2022-10-09 23:17:05 +0900

    :wrench:feat:presentation MainActivity 추가

---
+++ presentation/src/main/java/com/study/presentation/v2/activity/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.study.presentation.v2.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

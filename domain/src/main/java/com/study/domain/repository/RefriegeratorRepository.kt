8f62689ee09746479edea39be758b32caf5a774b
gjgustjd <gjgustjd70@gmail.com>
2022-10-14 15:12:03 +0900

    :seedling:feat:RefrigeratorRepository 메소드 suspend로 수정

--- domain/src/main/java/com/study/domain/repository/RefriegeratorRepository.kt
+++ domain/src/main/java/com/study/domain/repository/RefriegeratorRepository.kt
@@ -7,23 +7,24 @@ import com.study.domain.model.RecipeItem
 import kotlinx.coroutines.flow.Flow
 
 interface RefriegeratorRepository {
-    fun getIngredients(id: Int): List<IngredientItem>
-    fun insertIngredients(item: IngredientItem)
-    fun deleteIngredients(item: IngredientItem)
-    fun updateIngredients(item: IngredientItem)
-    fun getAllIngredient(): List<IngredientItem>
-    fun getAllIngredientByFlow(): Flow<List<IngredientItem>>
-    fun insertRecipe(item: RecipeItem)
-    fun deleteRecipe(item: RecipeItem)
-    fun updateRecipe(item: RecipeItem)
-    fun getAllRecipe(): List<RecipeItem>
-    fun getRecipeById(id: Int): RecipeItem
-    fun getRecipeByName(word: String): List<RecipeItem>
-    fun getRecipeByIngredients(word: String): List<RecipeItem>
-    fun getAllMeals(): List<MealItem>
-    fun getMealWithId(id: Int): MealItem
-    fun getAllMealsWithRecipe(): List<MealAndRecipeItem>
-    fun insertMeal(id: Int)
-    fun deleteMeal(item: MealItem)
-    fun deleteMealWithId(mealId: Int)
+    suspend fun getIngredientById(id: Int): IngredientItem?
+    suspend fun insertIngredients(item: IngredientItem)
+    suspend fun deleteIngredients(item: IngredientItem)
+    suspend fun updateIngredients(item: IngredientItem)
+    suspend fun getAllIngredient(): List<IngredientItem>
+    suspend fun getAllIngredientByFlow(): Flow<List<IngredientItem>>
+    suspend fun insertRecipe(item: RecipeItem)
+    suspend fun deleteRecipe(item: RecipeItem)
+    suspend fun updateRecipe(item: RecipeItem)
+    suspend fun getAllRecipe(): List<RecipeItem>
+    suspend fun getRecipeById(id: Int): RecipeItem
+    suspend fun getRecipeByName(word: String): List<RecipeItem>
+    suspend fun getRecipeByIngredients(word: String): List<RecipeItem>
+    suspend fun getAllMeals(): List<MealItem>
+    suspend fun getMealWithId(id: Int): MealItem
+    suspend fun getAllMealsWithRecipe(): List<MealAndRecipeItem>
+    suspend fun getAllMealsWithRecipeAsFlow():Flow<List<MealAndRecipeItem>>
+    suspend fun insertMeal(id: Int)
+    suspend fun deleteMeal(item: MealItem)
+    suspend fun deleteMealWithId(mealId: Int)
 }

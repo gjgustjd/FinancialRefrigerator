605122c36d52dbb6cf47fedc57f90d703d9b7160
gjgustjd <gjgustjd70@gmail.com>
2022-10-15 03:23:58 +0900

    :recycle:refactor:기존 Usecase 중 Flow 관련 메서드 수정

--- domain/src/main/java/com/study/domain/usecase/ingredients/GetAllIngredientsUseCase.kt
+++ domain/src/main/java/com/study/domain/usecase/ingredients/GetAllIngredientsUseCase.kt
@@ -1,17 +1,21 @@
 package com.study.domain.usecase.ingredients
 
+import com.study.domain.model.IngredientItem
 import com.study.domain.repository.RefrigeratorRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class GetAllIngredientsUseCase @Inject constructor(private val repository: RefrigeratorRepository) {
-    suspend fun execute(scope: CoroutineScope) =
-        withContext(scope.coroutineContext + Dispatchers.IO) {
-            repository.getAllIngredientByFlow()
+    fun execute(scope: CoroutineScope,collectProcess:(list:List<IngredientItem>)->Unit) =
+        scope.launch(Dispatchers.IO) {
+            repository.getAllIngredientByFlow().collectLatest {
+                collectProcess(it)
+            }
         }
 
-    suspend operator fun invoke(scope: CoroutineScope) =
-        execute(scope)
+    operator fun invoke(scope: CoroutineScope,collectProcess:(list:List<IngredientItem>)->Unit) =
+        execute(scope,collectProcess)
 }
--- domain/src/main/java/com/study/domain/usecase/meal/GetAllMealsWithRecipeUseCase.kt
+++ domain/src/main/java/com/study/domain/usecase/meal/GetAllMealsWithRecipeUseCase.kt
@@ -1,17 +1,21 @@
 package com.study.domain.usecase.meal
 
+import com.study.domain.model.MealAndRecipeItem
 import com.study.domain.repository.RefrigeratorRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class GetAllMealsWithRecipeUseCase @Inject constructor(private val repository: RefrigeratorRepository) {
-    suspend fun execute(scope: CoroutineScope) =
-        withContext(scope.coroutineContext + Dispatchers.IO) {
-            repository.getAllMealsWithRecipeAsFlow()
+    suspend fun execute(scope: CoroutineScope,collectProcess:(list:List<MealAndRecipeItem>)->Unit) =
+        scope.launch(Dispatchers.IO) {
+            repository.getAllMealsWithRecipeAsFlow().collectLatest {
+                collectProcess(it)
+            }
         }
 
-    suspend operator fun invoke(scope: CoroutineScope) =
-        execute(scope)
+    suspend operator fun invoke(scope: CoroutineScope,collectProcess:(list:List<MealAndRecipeItem>)->Unit) =
+        execute(scope,collectProcess)
 }
